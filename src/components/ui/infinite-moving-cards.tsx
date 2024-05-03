6ef1f77d21219cfafb039a710a466a85251e0c70
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2024-05-02 23:56:05 -0300

    feat: change logo carrousel

--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { cn } from "@/lib/utils"
+import Image from "next/image"
 import React, { useEffect, useState } from "react"
 
 export const InfiniteMovingCards = ({
@@ -87,13 +88,20 @@ export const InfiniteMovingCards = ({
       >
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
+            className="w-[250px] max-w-full relative rounded-2xl flex-shrink-0 px-8 py-6 md:w-[450px]"
             style={{
               background:
                 "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
             }}
             key={item.name}
-          ></li>
+          >
+            <Image
+              src={"/images/partner-example.png"}
+              alt="partner"
+              width={100}
+              height={100}
+            />
+          </li>
         ))}
       </ul>
     </div>
