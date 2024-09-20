688a3581dfb04c5077c196f0798f4685d1b64fd2
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2024-09-19 21:29:38 -0300

    feat: add urls

--- src/app/[slug]/components/Slider.tsx
+++ src/app/[slug]/components/Slider.tsx
@@ -27,7 +27,7 @@ const Slider = ({ imgs }: SliderProps) => {
             <CarouselItem key={index}>
               <figure className="w-full h-full flex items-center justify-center">
                 <Image
-                  src={"https://cejaspropiedades.com" + img}
+                  src={img}
                   alt="image-carrousel"
                   width={900}
                   height={900}
