10e72203ca813d4a4fb86c7b0a3946fc1ce4f0f5
tomaslami <tomaslamiguralnik@gmail.com>
2024-09-25 16:26:10 -0300

    feat: Gallery title fixed

--- src/app/[slug]/components/Gallery.tsx
+++ src/app/[slug]/components/Gallery.tsx
@@ -14,10 +14,12 @@ const Gallery = ({ imgs }: GalleryProps) => {
   return (
     <article className="w-full h-max flex items-center justify-center flex-col gap-12">
       <div className="w-full h-max flex items-center justify-center flex-col gap-4 ">
-        <Title className="flex flex-row items-center gap-3 text-black text-4xl sm:text-xl sm:text-center font-bold">
-          <IconSatellite size={40} className="text-black" />
+
+        <Title className="flex flex-row items-center gap-3 sm:w-[85%] text-black text-4xl sm:text-xl sm:text-center sm:gap-3 font-bold">
+          <IconSatellite size={40} className="text-black sm:hidden" />
           Imágenes Satelitales y Terrestres
         </Title>
+
         <p className="w-[40%] text-black/80 text-lg font-normal text-center 3xl:w-[80%] sm:text-xs">
           Explora la propiedad desde arriba y desde la tierra. Compare imágenes
           de satélite con fotografías de la vida real de los mismos lugares.
